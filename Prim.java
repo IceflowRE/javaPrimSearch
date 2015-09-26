37f228711735277b3a2db7d64a69a7d0bc1ea44d
Iceflower <cynosaura@gmail.com>
2015-09-26 14:24:43 +0200

    Finished fastCheckFile and error handling
    Some errors will be not handled atm (i dont need it atm) XD

--- Prim.java
+++ Prim.java
@@ -2,48 +2,53 @@
 * Author: Iceflower S
 */
 
-import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
 
-import com.sun.javafx.css.CascadingStyle;
-
-import javafx.scene.shape.Line;
-
-import java.io.*;
-import java.nio.file.Path;
-//fIn.getChannel().position(0);
-//bRead = new BufferedReader(new InputStreamReader(fIn));
 public class Prim {
 	public static void main(String[] args) throws IOException {
 		String dir = "data";
 		String file = "primes.txt";
 		File primStorage = new File(dir + "/" + file);
 
-		if (primStorage.exists()) {
-			System.out.println("filescheck done");
+		if (primStorage.exists()) { //filecheck
+			System.out.println("DONE: filescheck");
 		} else { //show message with option to create them
+			System.out.println("FAILED: filescheck");
 			{ //option dialog
 				int choosen_temp = JOptionDialog("Missing files!", "Try to create them?", 2, new Object[] {"Yes", "No & Exit"});
 				if(choosen_temp == -1 || choosen_temp == 1) {
 					System.exit(0);
 				} else {
 					if ( createDir(dir, file) ) { //create file
+						System.out.println("DONE: files created");
 						JOptionDialog("", "Succesfull created", 1, new Object[] {"Ok"});
 					} else {
+						System.out.println("FAILED: creating files");
 						JOptionDialog("Error", "A problem occurred (check write access and execute files)", 0, new Object[] {"Ok & Exit"});
 						System.exit(0);
 					}
 				}
 			}
 		} //end error handling
-		long[] stats = fastCheckPrimList(dir + "/" + file);
-		if (stats[2] < 0) {
+		long[] stats = fastCheckPrimList(dir + "/" + file); //fastlistcheck
+		if (stats[2] < 0) { //work with the fastcheck result
+			System.out.println("FAILED: fast primeslist check" + "at prim: " + stats[1] + " (line: " + stats[0] + ")");
 			if ( repairPrimList(stats, dir, file) == false) {
-				System.out.println("Error while repairing");
+				System.out.println("FAILED: primelist recreating/ repairing");
 				System.exit(0);
 			}
+			System.out.println("DONE: primelist recreating/ repairing");
 		} else {
-			System.out.println("fast primlistcheck done");
+			System.out.println("DONE: fast primeslist check");
 		}
+		
+		JOptionPane.showInputDialog("Suche von " + stats[1] + " um X Zahlen weiter:");
 	}
 
 	public static boolean createDir(String dir, String file) throws IOException {
@@ -54,21 +59,27 @@ public class Prim {
 				return false;
 			}
 		}
-		fileFile.createNewFile();
-		//add first prim 2
-		FileWriter fw = new FileWriter(fileFile);
-		BufferedWriter bw = new BufferedWriter(fw);
-		bw.write("2" + "\n");
-		bw.close();
+		try {
+			fileFile.delete();
+			fileFile.createNewFile();
+			//add first prim 2
+			FileWriter fw = new FileWriter(fileFile);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write("2" + "\n");
+			bw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return true;
 	}
 	public static long[] fastCheckPrimList(String relPath) { //long: 0 = correct prim; 1 = biggest prim; 2 = status (-2 = recreate all, -1 = error file need to be cutted, 1 = all ok)
-		long correctPrim = 0, biggestPrim = 0, status = 0;
+		long correctPrim = 0, biggestPrim = 0;
 		try {
 			BufferedReader bRead = new BufferedReader(new FileReader(new File(relPath)));
 			String curLine = bRead.readLine();
 			long curPrim;
 			if ( (curLine == null) || ((curPrim = Long.parseLong(curLine)) != 2)) {//first check
+				bRead.close();
 				return new long[] {correctPrim, biggestPrim, -2};
 			}
 			biggestPrim = 2;
@@ -80,8 +91,8 @@ public class Prim {
 					bRead.close();
 					return new long[] {correctPrim, biggestPrim, -1};
 				}
-				bRead.close();
 			}
+			bRead.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(0);
@@ -95,24 +106,22 @@ public class Prim {
 		File primStorage = new File(dir + "/" + file);
 		switch (statusStr) {
 		case "-2": {
-				choosen_temp = JOptionDialog("Error in the primnumber list", "List will be recreated (you will lose maybe some primnumbers)", 2, new Object[] {"Yes", "No & Exit"});
+				choosen_temp = JOptionDialog("Error in the prime list", "List will be recreated (you will lose all primes)", 2, new Object[] {"Yes", "No & Exit"});
 				if(choosen_temp == -1 || choosen_temp == 1) { //close
 					return false;
 				} else { //try to repair
-					if (primStorage.delete()) { //need work
-						try {
-							createDir(dir, file);
-						} catch (Exception e) {
-							e.printStackTrace();
-							choosen_temp = JOptionDialog("Error while recreating", "Try to deleted (.../" + (dir + "/" + file) +")", 2, new Object[] {"Ok & Exit"});
-							return false;
-						}
+					try {
+						createDir(dir, file);
+					} catch (Exception e) {
+						e.printStackTrace();
+						choosen_temp = JOptionDialog("Error while recreating", "Try to deleted (.../" + (dir + "/" + file) +")", 2, new Object[] {"Ok & Exit"});
+						return false;
 					}
 				}//end error handling
 				JOptionDialog("", "Succesfull recreated", 1, new Object[] {"Ok"});
 				return true; 
 		} case "-1": {
-				choosen_temp = JOptionDialog("Primnumber list arent correct", "Try to repair it? (you will lose maybe some primnumbers)", 2, new Object[] {"Yes", "No & Exit"});
+				choosen_temp = JOptionDialog("Prime list arent correct", "Try to repair it? (you will lose maybe some primes)", 2, new Object[] {"Yes", "No & Exit"});
 				if (choosen_temp == -1 || choosen_temp == 1) { //close
 					return false;
 				} else { //try to repair need work
@@ -121,21 +130,22 @@ public class Prim {
 						BufferedReader bRead = new BufferedReader(new FileReader(primStorage));
 						BufferedWriter bWrite = new BufferedWriter(new FileWriter(copyTemp));
 						System.out.println(stats[0]);
+						int progress = 0;
+						double corPrimDou = (double) stats[0];
 						for (long i = 1; i <= stats[0]; i++) {
 							long curPrim = Long.parseLong(bRead.readLine());
-							System.out.println(i);
 							bWrite.write(curPrim + "\n");
+							int progtemp = (int) ((100 / corPrimDou) * i);
+							if (progtemp != progress) {
+								System.out.println("List repairing: " + progtemp + "%");
+								progress = progtemp;
+							}
 						}
 						bWrite.close();
 						bRead.close();
-						BufferedReader bRead2 = new BufferedReader(new FileReader(copyTemp));
-						BufferedWriter bWrite2 = new BufferedWriter(new FileWriter(primStorage));
-						String curLine;
-						for (long i = 1; ((curLine = bRead2.readLine()) != null); i++) {
-							long curPrim = Long.parseLong(curLine);
-							bWrite2.write(curPrim + "\n");
-						}
-						
+						primStorage.delete();
+						copyTemp.renameTo(primStorage);
+						copyTemp.delete();
 						stats = new long[] {stats[0], stats[1], 1};
 					} catch (Exception e) {
 						choosen_temp = JOptionDialog("Error while reparing", "Try to deleted (.../" + (dir + "/" + file) +")", 2, new Object[] {"Ok & Exit"});
