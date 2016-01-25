f162293ab87dff9d74c9be600c07d008d2e8ad7c
Iceflower <cynosaura@gmail.com>
2016-01-25 14:22:43 +0100

    use new structure
    - comes from the multiCalc

--- src/PrimeCalc.java
+++ src/PrimeCalc.java
@@ -26,9 +26,7 @@ public class PrimeCalc {
 		long time1 = System.nanoTime();
 		try {
 			for (long i = Main.biggestPrim + 2; i <= upTo; i += 2) { //loop for testing the number and give them to the distributor which will test them with MT
-				distributor(i); //Multi
-				if (isCurTestPrime) { //Multi
-				//if (isPrimWithBigList(i) == true) { //single
+				if (isPrimWithBigList(i) == true) { //single
 					Main.primList.add(i);
 				}
 			}
@@ -38,40 +36,6 @@ public class PrimeCalc {
 			e.printStackTrace();
 			Main.JOptionDialog("Error", "An undetected problem occurred, test primes isnt finished\ntry to save the found primes", 0, new Object[] {"Ok"});
 		}
-	}
-
-	private final static void distributor(long curTest) { //alternative a fixed parts size; tests the number like this easy e.g. test 800: the primlist is 80 large, so Thread1 will test primList 0-10, Thread2 11-20, and so on. the eachPart size is here 10
-		int useThreads = 1;
-		int start;
-		isCurTestPrime = true; //if the number is a prim, at start its a prim until you check its not a prime
-		if (Main.primList.size() < Main.useMTifIndices) {// use MT only if there are enough index's, maybe later calc the best threads quantity until maxThreads which you are use
-			useThreads = 1;
-		} else {
-			useThreads = Main.maxCalcThreads;
-		}
-		ExecutorService pool = Executors.newFixedThreadPool(Main.maxCalcThreads);
-		System.out.println(Main.primList); //DEV
-		System.out.println("MT starts - " + curTest); //DEV
-		start = 0;
-		Runnable worker = new PrimeDistributor(curTest, start, useThreads); //init the first worker, at least there is every time one worker
-		pool.execute(worker);
-		for (int i = 1; i < useThreads; i++) { // Main.maxCalcThreads = 3: thread1 tests: 0,3,6,9,... thread2 tests: 1,4,7,10,... thread3 tests: 2,5,8,11,...  (numbers are index of primList)
-			start = i;
-			worker = new PrimeDistributor(curTest, start, useThreads);
-			pool.execute(worker);
-		}
-		pool.shutdown();
-		// Wait until all threads have finished
-		while(!pool.isTerminated()) {
-			if (!isCurTestPrime) {
-				pool.shutdownNow(); //needed because of flag?
-				break;
-			}
-		}
-		while(!pool.isTerminated()) { //needed?
-
-		}
-		System.out.println("MT ends - " + curTest); //DEV
 	}
 	
 	private final static boolean isPrimWithFile(long curTest) throws IOException { //dont forget flushing if the sqrtValue isnt in the list
@@ -120,39 +84,4 @@ public class PrimeCalc {
 		}
 		return true;
 	}	
-}
-
-class PrimeDistributor extends Thread {
-	private long curTest;
-	private int start;
-	private int listSize;
-	private long useThreads;
-	
-	PrimeDistributor(long curTest, int start, long useThreads) {
-		if (!PrimeCalc.isCurTestPrime) { //makes the shutdown faster
-			return;
-		}
-		this.curTest = curTest;
-		this.start = start;
-		this.listSize = Main.primList.size();
-		this.useThreads = useThreads;
-	}
-
-	@Override
-	public final void run() {
-		long curPrim = 0;
-		long sqrt = (long) Math.sqrt(curTest);
-		for (int i = this.start; (i < this.listSize) && ((curPrim = Main.primList.get(i)) <= sqrt); i += this.useThreads) {
-			System.out.println(this.getName() + " - " + curTest + " - " + curPrim);
-			if (!PrimeCalc.isCurTestPrime) { //makes the shutdown faster
-				return;
-			}
-			if (this.curTest % curPrim == 0) {
-				System.out.println("no prim");
-				PrimeCalc.isCurTestPrime = false; //change this boolean to shutdown all other threads via shutdownNow()
-				return;
-			}
-		}
-		return;
-	}
 }
